68e8d31a063dc17835bcd63b37a74b7863aa014f
hobby8 <hobby8@users.noreply.github.com>
2018-07-27 10:43:12 +0300

    Windows DPI unaware on Windows 8.1+
    Programmatically specifying DPI aware only on Windows 8.0 and earlier

--- src/SFML/Window/Win32/WindowImplWin32.cpp
+++ src/SFML/Window/Win32/WindowImplWin32.cpp
@@ -90,7 +90,11 @@ namespace
                 // We only check for E_INVALIDARG because we would get
                 // E_ACCESSDENIED if the DPI was already set previously
                 // and S_OK means the call was successful
-                if (SetProcessDpiAwarenessFunc(ProcessSystemDpiAware) == E_INVALIDARG)
+
+// NOTE: THE FOLLOWING HAS BEEN MODIFIED SO THAT ON WINDOWS 8.1+, PROCESS DECLARES ITSELF
+// DPI-UNAWARE, AND ON EARLIER WINDOWS VERSIONS, DPI-AWARE
+                //if (SetProcessDpiAwarenessFunc(ProcessSystemDpiAware) == E_INVALIDARG)
+                if (SetProcessDpiAwarenessFunc(ProcessDpiUnaware) == E_INVALIDARG)
                 {
                     sf::err() << "Failed to set process DPI awareness" << std::endl;
                 }
